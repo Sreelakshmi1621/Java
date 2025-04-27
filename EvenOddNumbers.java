f93c6be94797e281ce31fd2fa8e16c2b08fc985a
Sreelakshmi1621 <ammuponnu162131@gmail.com>
2025-04-28 00:03:24 +0530

    Add files via upload

---
+++ EvenOddNumbers.java
@@ -0,0 +1,32 @@
+import java.io.*; 
+import java.util.Scanner; 
+public class EvenOddNumbers { 
+public static void main(String[] args) { 
+try { 
+Scanner inputScanner = new Scanner(System.in); 
+System.out.print("Enter the name of the input file: "); 
+String inputFileName = inputScanner.nextLine(); 
+FileInputStream inputFile = new FileInputStream(inputFileName); 
+Scanner fileScanner = new Scanner(inputFile); 
+FileOutputStream evenFile = new FileOutputStream("even.txt"); 
+FileOutputStream oddFile = new FileOutputStream("odd.txt"); 
+PrintWriter evenWriter = new PrintWriter(evenFile); 
+PrintWriter oddWriter = new PrintWriter(oddFile); 
+while (fileScanner.hasNextInt()) { 
+int number = fileScanner.nextInt(); 
+if (number % 2 == 0) { 
+evenWriter.println(number); // Write even numbers to the even file 
+} else { 
+oddWriter.println(number);  // Write odd numbers to the odd file
+} 
+} 
+evenWriter.close(); 
+oddWriter.close(); 
+inputFile.close(); 
+System.out.println("Even and odd numbers have been copied to separate files."); 
+} catch (IOException e) { 
+System.err.println("Error: " + e.getMessage()); 
+} 
+} 
+} 
+
